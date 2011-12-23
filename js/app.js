1c2194dab3fc3d56c15afd0fc34aab3df63d4a15
Greg <gsmith@incompl.com>
2011-12-23 18:34:39 -0500

    more tweaks

---
+++ js/app.js
@@ -0,0 +1,13 @@
+(function() {
+
+  if (Modernizr.cssanimations && Modernizr.csstransforms) {
+    document.getElementById("circles").style.display = "block";
+  }
+  
+  var contact = document.getElementById("contact")
+  var email = ["om", ".c", "pl", "om", "nc", "@i", "th", "mi", "gs"].reverse().join("");
+  contact.style.display = "inline";
+  contact.innerHTML = email;
+  contact.href = "mailto:" + email;
+
+})();
